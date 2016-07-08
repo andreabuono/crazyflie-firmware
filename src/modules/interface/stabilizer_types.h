1dfa99c23e150f2657394cd986ee0b6cbe6873f8
Mike Hamer <mike@mikehamer.info>
2016-07-08 16:19:04 +0200

    Kalman Filter
    The Kalman filter still needs a lot of tuning (process and measurement noises).
    
    In addition, there are many TODOs, see estimator_kalman.c for more details.
    
    Of particular note are the following issues, which should be investigated
    
    1. Aggressive takeoff makes the quad behave strangely
    2. When sitting on the ground (quadIsFlying == false), the accelerometer gives information about the attitude of the quad, which could be incorperated into the estimate. Currently the attitude is initialized as zero, and the quad needs to be started flat.
    3. Extensive testing is needed, sometimes the estimate or covariance matrix goes haywire
    
    Signed-off-by: Mike Hamer <mike@mikehamer.info>

--- src/modules/interface/stabilizer_types.h
+++ src/modules/interface/stabilizer_types.h
@@ -76,11 +76,41 @@ typedef struct quaternion_s {
   };
 } quaternion_t;
 
+typedef struct uwbPacket_s {
+  float x, y, z;
+  int64_t rx, tx;
+} uwbPacket_t;
+
 typedef struct baro_s {
   float pressure;
   float temperature;
   float asl;
 } baro_t;
+
+typedef struct positionMeasurement_s {
+  union {
+    struct {
+      float x;
+      float y;
+      float z;
+    };
+    float pos[3];
+  };
+  float stdDev;
+} positionMeasurement_t;
+
+typedef struct distanceMeasurement_s {
+  union {
+    struct {
+      float x;
+      float y;
+      float z;
+    };
+    float pos[3];
+  };
+  float distance;
+  float stdDev;
+} distanceMeasurement_t;
 
 typedef struct sensorData_s {
   Axis3f acc;
@@ -92,6 +122,7 @@ typedef struct sensorData_s {
 
 typedef struct state_s {
   attitude_t attitude;
+  quaternion_t attitudeQuaternion;
   point_t position;
   velocity_t velocity;
   acc_t acc;
@@ -145,8 +176,14 @@ typedef struct setpointZ_s {
 #define RATE_500_HZ 500
 #define RATE_250_HZ 250
 #define RATE_100_HZ 100
+#define RATE_50_HZ 50
+#define RATE_25_HZ 25
 
+#ifdef ESTIMATOR_TYPE_kalman
 #define RATE_MAIN_LOOP RATE_1000_HZ
+#else
+#define RATE_MAIN_LOOP RATE_1000_HZ
+#endif
 
 #define RATE_DO_EXECUTE(RATE_HZ, TICK) ((TICK % (RATE_MAIN_LOOP / RATE_HZ)) == 0)
 
